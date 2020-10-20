cdb5db66d2c5696c49d122714d475e29ce62273a
Nixon Andhika <nixonandhika28@gmail.com>
2020-10-21 01:10:50 +0700

    typo

--- feistel.py
+++ feistel.py
@@ -1,17 +1,15 @@
 from helper import Helper as helper
-from keyGenerator import KeyGenerator as keygen
+from keyGenerator import KeyGenerator
 import numpy as np
 from fFunction import fFunc
 
 def feistel(leftBlock, rightBlock, keygen, itr):
     if (itr > 0):
-        # print(itr)
         keygen.round()
         i = itr-1
         print(keygen.subKey, keygen.crossKey)
         feistel(rightBlock, helper.xor(leftBlock, fFunc(rightBlock, keygen.subKey, keygen.crossKey)), keygen, i)
     else:
-        # print(itr)
         return helper.convertBinary64ToString(leftBlock + rightBlock)
 
 if __name__ == "__main__":
@@ -19,6 +17,6 @@ if __name__ == "__main__":
     left = stringBlock[:32]
     right = stringBlock[-32:]
     keyBlock = helper.convertStringToBinary64("qwertyui")[0]
-    keygen = keygen(keyBlock, 1, 2)
+    keygen = KeyGenerator(keyBlock, 1, 2)
     np.random.seed(helper.totalAsciiCode("qwertyui"))
     feistel(left, right, keygen, 8)
