2c3eda2415579e9888ab68e0b1b243a42cf897f3
unknown <saragih.meyer0821@gmail.com>
2020-10-21 10:41:59 +0700

    fix key generator

--- keyGenerator.py
+++ keyGenerator.py
@@ -12,6 +12,7 @@ class KeyGenerator:
         self.sbox2 = SBox(seed2)
         self.subKey = None
         self.crossKey = None
+        self.round_count = 0
     
     def round(self):
         # Key Split
@@ -32,17 +33,29 @@ class KeyGenerator:
 
         self.subKey = left_key[0:8] + right_key[16:24]
         self.crossKey = left_key[8:24] + right_key[0:16]
-        self.key = self.crossKey
-        
+        self.generate_next_key()
+        self.round_count += 1
+    
+    def generate_next_key(self):
+        pick = self.round_count % 3
+        self.key = self.subKey + self.crossKey
+        for i in range(0, len(self.key), 3):
+            self.key += self.key[i + pick]
 
 if __name__ == "__main__":
     test_case = 'waef290e'
     test_case = h.convertStringToBinary64(test_case)[0]
 
     keygen = KeyGenerator(test_case, 1, 2)
+    print(keygen)
     keygen.round()
 
     print(keygen.subKey)
     print(keygen.crossKey)
+
+    keygen.round()
+    print(keygen.subKey)
+    print(keygen.crossKey)
+
 
 
